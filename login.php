b3a38c1785bf5ce8190232b60cc71f70d5ecdb0b
MisterGit <anass.el-gharbaoui@univ-rouen.fr>
2022-12-12 18:53:23 +0100

    setting up and fix front

--- login.php
+++ login.php
@@ -1,22 +1,29 @@
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>kanban login</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="../css/login.css" rel="stylesheet">
   </head>
   <body>
     <section class="vh-100">
       <div class="container-fluid h-custom">
-        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100 mt-6">
           <div class="col-md-9 col-lg-6 col-xl-5">
             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
               class="img-fluid" alt="Sample image">
           </div>
           <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-            <form method="post" action="verifylogin.php">
+            <div class="mx-auto mb-4" style="width: 200px;">
+              <h1>Connexion</h1>  
+            </div>
+            <form  method="post" action="verifylogin.php">
     
               <!-- Email input -->
               <div class="form-outline mb-4">
@@ -42,7 +49,7 @@
               </div>
     
               <div class="text-center mx-auto mt-4 pt-2">
-                <input id="connect" type="submit" name="formconnexion" value="Se connecter "/>
+                <button class="btn btn-dark btn-lg" type="submit" name="formconnexion">Se connecter</button>
                 <p class="small fw-bold mt-4 pt-1 mb-0">Vous n'avez pas de compte ?  <a href="./authentification.php" class="link-danger">S'authentififer</a></p>
               </div>  
             </form>
@@ -50,9 +57,9 @@
         </div>
       </div>
       <div
-        class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-dark">
+        class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-transparent text-dark">
         <!-- Copyright -->
-        <div class="text-white mx-auto mb-3 mb-md-0 align-items-center">
+        <div class="mx-auto mb-3 mb-md-0 align-items-center">
           Copyright © 2022. tous les droits sont réservés.
         </div>
         <!-- Copyright -->
@@ -63,4 +70,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
+
 
