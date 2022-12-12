090ecadc105aab04e72bf88f3a7e14283e0ea4ec
MisterGit <anass.el-gharbaoui@univ-rouen.fr>
2022-12-12 21:07:04 +0100

    userKanban page

--- userKanban.php
+++ userKanban.php
@@ -1,6 +1,3 @@
-<?php 
-//verify login if ok redirect to user page else redirect to login page
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +8,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="./css/index.css" rel="stylesheet" type="text/css" media="screen">
-    <title>Rendez Vous</title>
+    <title>MesKanbans</title>
 
 </head>
 <body>
@@ -22,7 +19,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-        <a type="button" class="btn btn-success mr-2" href="deconnexion.php">Se Déconnecter</a>
+        <a type="button" class="btn btn-success mr-2" href="./templates/login.html">Se Connecter</a>
     </div>
 </nav>
 
@@ -32,15 +29,15 @@
             <div class="sidebar-sticky">
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">
+                        <a class="nav-link" href="userPage.php">
                             <span data-feather="home"></span>
-                            Kanban<span class="sr-only">(current)</span>
+                            Kanban
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="userKanban.php">
+                        <a class="nav-link active" href="#">
                             <span data-feather="home"></span>
-                            Mes Kanbans
+                            Mes Kanbans<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -52,7 +49,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">
                             <span data-feather="home"></span>
-                            Créer des Kanbans
+                            créer un kanban
                         </a>
                     </li>
                 </ul>
@@ -63,29 +60,32 @@
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
                 <h1 class="h2">List des kanbans</h1>
             </div>
-            <div class="row-content">
-                <div class="card-columns">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">kanban</h5>
-                            <p class="card-text">kanban description</p>
-                        </div>
-                        <div class="card-footer text-white bg-dark">
-                            <div class="btn-toolbar justify-content-end">
-                                <div class="btn-group mr-2">
-                                    
-                                    <a class="btn btn-sm btn-outline-success" href="#"><span data-feather="eye"></span></a>
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="card bg-light">
+                    <div class="card-body">
+                        <h6 class="card-title text-uppercase text-truncate py-2">To Do</h6>
+                        <div class="items border border-light">
+                            <div class="card draggable shadow-sm" id="" draggable="true" ondragstart="">
+                                <div class="card-body p-2">
+                                    <div class="card-title">
+                                        <p class="text-muted float-right">date</p>
+                                        <a href="" class="lead font-weight-light">TSK-154</a>
+                                    </div>
+                                    <p>
+                                        This is a description of a item on the board.
+                                    </p>
+                                    <button class="btn btn-primary btn-sm">View</button>
                                 </div>
                             </div>
+                            <div class="dropzone rounded" ondrop="" ondragover="" ondragleave=""> &nbsp; </div>
                         </div>
                     </div>
                 </div>
             </div>
+           
         </main>
     </div>
 </div>
-
-
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
