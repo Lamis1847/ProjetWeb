5981aee6542b67e55cc971f96f5bc5e12e64020b
Lamis1847 <49839931+Lamis1847@users.noreply.github.com>
2022-12-20 17:10:51 +0100

    Update projet_web.sql

--- projet_web.sql
+++ projet_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 19 nov. 2022 à 13:51
+-- Généré le :  mar. 20 déc. 2022 à 16:09
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -32,9 +32,24 @@ DROP TABLE IF EXISTS `kanban`;
 CREATE TABLE IF NOT EXISTS `kanban` (
   `Id` int(255) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `visibilite` enum('public','prive') COLLATE utf8mb4_bin NOT NULL,
+  `IdCreateur` int(255) NOT NULL,
+  `nbTable` int(255) NOT NULL,
+  `Statut1` enum('A_FAIRE','EN_COURS','TERMINE') COLLATE utf8mb4_bin NOT NULL,
+  `Statut2` enum('A_FAIRE','EN_COURS','TERMINE') COLLATE utf8mb4_bin NOT NULL,
+  `Statut3` enum('A_FAIRE','EN_COURS','TERMINE') COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `kanban`
+--
+
+INSERT INTO `kanban` (`Id`, `titre`, `description`, `visibilite`, `IdCreateur`, `nbTable`, `Statut1`, `Statut2`, `Statut3`) VALUES
+(1, 'kanban', 'kanban description', 'public', 1, 0, 'A_FAIRE', 'A_FAIRE', 'A_FAIRE'),
+(2, 'ABCD', 'kanban 2 description', 'public', 1, 0, 'A_FAIRE', 'A_FAIRE', 'A_FAIRE'),
+(3, 'news9', 'hello', 'public', 1, 1, 'A_FAIRE', 'A_FAIRE', 'A_FAIRE');
 
 -- --------------------------------------------------------
 
@@ -48,7 +63,15 @@ CREATE TABLE IF NOT EXISTS `participant` (
   `Iduser` int(255) NOT NULL,
   `Idkanban` int(255) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `participant`
+--
+
+INSERT INTO `participant` (`Id`, `Iduser`, `Idkanban`) VALUES
+(1, 1, 1),
+(2, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -64,8 +87,17 @@ CREATE TABLE IF NOT EXISTS `tache` (
   `idKanban` int(255) NOT NULL,
   `idUser` int(255) NOT NULL,
   `dateLimite` date NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `tache`
+--
+
+INSERT INTO `tache` (`Id`, `titre`, `description`, `idKanban`, `idUser`, `dateLimite`, `status`) VALUES
+(1, 'tache1', 'description tache', 1, 1, '2022-12-15', 'affectee'),
+(2, 'tache2', 'description tache', 2, 1, '2022-12-14', 'affectee');
 
 -- --------------------------------------------------------
 
@@ -81,8 +113,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `motDePasse` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `userName` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `numero` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`Id`, `nom`, `prenom`, `email`, `motDePasse`, `userName`, `numero`) VALUES
+(1, 'boufafa', 'lamis', 'lamisboufafa@esi.dz', '0000', 'lamis', ''),
+(3, 'Chebbi', 'Lakhdar', 'lakhdar7606@gmail.com', '123', 'chebbi', '0745642982'),
+(6, 'test', 'test', 'test@esi.dz', '$1$Cx/ip1vO$5J.D8XUv6aR1ivn8ojh6H.', 'test', '0553351917'),
+(10, 'test', 'test', 'test', 'test', '', ''),
+(9, 'imed', 'imed', 'imed@esi.dz', '$1$C4Cqe8h.$uiNNCw9.i.RWOvDXjngvq1', 'imed', '0553351917');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
